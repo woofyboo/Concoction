e2968d35a8480769e2be9ca26e5882672f55ac5b
Sebsish <se6sish@gmail.com>
2024-11-04 22:34:11 +0300

    half-comlpeted work with sunflower

--- src/main/java/net/mcreator/concoction/block/SunflowerBlock.java
+++ src/main/java/net/mcreator/concoction/block/SunflowerBlock.java
@@ -8,31 +8,43 @@ import net.minecraft.world.level.pathfinder.PathType;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.util.RandomSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.SpecialPlantable;
 
 import net.mcreator.concoction.procedures.SunflowerOnTickUpdateProcedure;
 import net.minecraft.world.level.block.CropBlock;
 
 public class SunflowerBlock extends CropBlock {
 	public static final EnumProperty<FacingProperty> FACING = EnumProperty.create("facing", FacingProperty.class);
-	public static final int MAX_AGE = 5;
+	public static final int FIRST_STAGE_MAX_AGE = 2;
+    public static final int SECOND_STAGE_MAX_AGE = 3;
+    public static final int MAX_AGE = FIRST_STAGE_MAX_AGE + SECOND_STAGE_MAX_AGE;
 	public static final IntegerProperty AGE = IntegerProperty.create("age", 0, MAX_AGE);
-	public static final EnumProperty<HalfProperty> HALF = EnumProperty.create("half", HalfProperty.class);
+	public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
 	
-    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
             Block.box(6.0D, 0.0D, 6.0D, 10.0D, 8.0D, 10.0D),
             Block.box(4.0D, 0.0D, 4.0D, 12.0D, 14.0D, 12.0D),
             Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
@@ -42,7 +54,7 @@ public class SunflowerBlock extends CropBlock {
   
 	public SunflowerBlock() {
 		super(BlockBehaviour.Properties.of().mapColor(MapColor.GRASS).sound(SoundType.GRASS).strength(0f, 10f).noCollission().noOcclusion().randomTicks().pushReaction(PushReaction.DESTROY).isRedstoneConductor((bs, br, bp) -> false));
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, FacingProperty.EAST).setValue(this.getAgeProperty(), Integer.valueOf(0)).setValue(HALF, HalfProperty.BOTTOM));
+		this.registerDefaultState(this.stateDefinition.any().setValue(this.getAgeProperty(), Integer.valueOf(0)).setValue(FACING, FacingProperty.EAST).setValue(HALF, DoubleBlockHalf.LOWER));
 	}
 
 	@Override
@@ -66,19 +78,77 @@ public class SunflowerBlock extends CropBlock {
         return SHAPE_BY_AGE[this.getAge(pState)];
     }
 
+	@Override
+    public void growCrops(Level pLevel, BlockPos pPos, BlockState pState) {
+        int nextAge = this.getAge(pState) + this.getBonemealAgeIncrease(pLevel);
+        int maxAge = this.getMaxAge();
+        if(nextAge > maxAge) {
+            nextAge = maxAge;
+        }
+        if (nextAge >= FIRST_STAGE_MAX_AGE && pState.getValue(HALF) == DoubleBlockHalf.LOWER && pLevel.getBlockState(pPos.above(1)).is(Blocks.AIR)) {
+        	
+            pLevel.setBlock(pPos.above(1), this.getState(pState, nextAge - FIRST_STAGE_MAX_AGE, DoubleBlockHalf.UPPER), 2);
+            pLevel.setBlock(pPos, this.getState(pState, FIRST_STAGE_MAX_AGE, DoubleBlockHalf.LOWER), 2);
+            
+        } else if (nextAge < FIRST_STAGE_MAX_AGE && pState.getValue(HALF) == DoubleBlockHalf.LOWER) {
+            pLevel.setBlock(pPos, this.getState(pState, nextAge, DoubleBlockHalf.LOWER), 2);
+            
+        }
+    }
 
+    public BlockState getState(BlockState pState, int age, DoubleBlockHalf half) {
+        return this.defaultBlockState().setValue(this.getAgeProperty(), Integer.valueOf(age)).
+        		setValue(FACING, pState.getValue(FACING)).setValue(HALF, half);
+    }
+    
+//    @Override
+//    public boolean isValidBonemealTarget(LevelReader pLevel, BlockPos pPos, BlockState pState) {
+//    		if (pState.getValue(HALF) == DoubleBlockHalf.LOWER)
+//    			return pState.getValue(AGE) != FIRST_STAGE_MAX_AGE;
+//    		else if (pState.getValue(HALF) == DoubleBlockHalf.UPPER)
+//    			return pState.getValue(AGE) != SECOND_STAGE_MAX_AGE;
+//		else
+//        		return !this.isMaxAge(pState);
+//    }
+    
+    @Override
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        return super.canSurvive(pState, pLevel, pPos) || (pLevel.getBlockState(pPos.below(1)).is(this) &&
+                pLevel.getBlockState(pPos.below(1)).getValue(AGE) == FIRST_STAGE_MAX_AGE);
+    }
+
+    @Override
+    public boolean mayPlaceOn(BlockState pState, BlockGetter p_52303_, BlockPos p_52304_) {
+        return pState.is(BlockTags.DIRT) || pState.getBlock() instanceof net.minecraft.world.level.block.FarmBlock;
+    }
 
 	@Override
+	public void destroy(LevelAccessor level, BlockPos pPos, BlockState pState) {
+	    Level pLevel = (Level)level;
+	    
+	    	if (!pLevel.isClientSide) {
+		        if (pState.getValue(HALF) == DoubleBlockHalf.UPPER) {
+		           BlockState blockBelow = pLevel.getBlockState(pPos.below(1));
+	
+		           if (blockBelow.getBlock() == pState.getBlock() && blockBelow.getValue(HALF) == DoubleBlockHalf.LOWER) {
+		              pLevel.setBlock(pPos.below(1), Blocks.AIR.defaultBlockState(), 35);
+		           }
+		        }
+	    		}
+    }
+    
+	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		//super.createBlockStateDefinition(builder);
-		builder.add(FACING, AGE, HALF);
+		builder.add(AGE, FACING, HALF);
+//		super.createBlockStateDefinition(builder);
 	}
 
-//	@Override
-//	public BlockState getStateForPlacement(BlockPlaceContext context) {
-//		return super.getStateForPlacement(context).setValue(FACING, FacingProperty.EAST).setValue(AGE, 0).setValue(HALF, HalfProperty.BOTTOM);
-//	}
-//
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return super.getStateForPlacement(context).setValue(this.getAgeProperty(), Integer.valueOf(0)).
+				setValue(FACING, FacingProperty.EAST).setValue(HALF, DoubleBlockHalf.LOWER);
+	}
+
 	@Override
 	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 100;
@@ -92,30 +162,24 @@ public class SunflowerBlock extends CropBlock {
 	@Override
 	public void randomTick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
 		super.randomTick(blockstate, world, pos, random);
-		SunflowerOnTickUpdateProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), blockstate);
+		SunflowerOnTickUpdateProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
 	}
-
+	
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+    
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+    
 	public enum FacingProperty implements StringRepresentable {
 		EAST("east"), EASTISH("eastish"), WESTISH("westish"), WEST("west");
 
 		private final String name;
 
 		private FacingProperty(String name) {
-			this.name = name;
-		}
-
-		@Override
-		public String getSerializedName() {
-			return this.name;
-		}
-	}
-
-	public enum HalfProperty implements StringRepresentable {
-		BOTTOM("bottom"), UPPER("upper");
-
-		private final String name;
-
-		private HalfProperty(String name) {
 			this.name = name;
 		}
 
--- src/main/java/net/mcreator/concoction/procedures/SunflowerOnTickUpdateProcedure.java
+++ src/main/java/net/mcreator/concoction/procedures/SunflowerOnTickUpdateProcedure.java
@@ -1,17 +1,12 @@
 package net.mcreator.concoction.procedures;
 
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.concoction.init.ConcoctionModBlocks;
-
 public class SunflowerOnTickUpdateProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate) {
+	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if (world.canSeeSkyFromBelowWater(BlockPos.containing(x, y + 2, z))) {
 			if (world.dayTime() >= 0 && world.dayTime() < 3000) {
 				{
@@ -45,114 +40,6 @@ public class SunflowerOnTickUpdateProcedure {
 					if (_bs.getBlock().getStateDefinition().getProperty("facing") instanceof EnumProperty _enumProp && _enumProp.getValue(_value).isPresent())
 						world.setBlock(_pos, _bs.setValue(_enumProp, (Enum) _enumProp.getValue(_value).get()), 3);
 				}
-			}
-		}
-		if (Math.random() < 1 || new Object() {
-			public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-				BlockEntity blockEntity = world.getBlockEntity(pos);
-				if (blockEntity != null)
-					return blockEntity.getPersistentData().getDouble(tag);
-				return -1;
-			}
-		}.getValue(world, BlockPos.containing(x, y, z), "attempts") >= 30) {
-			if ((blockstate.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip15 ? blockstate.getValue(_getip15) : -1) == 0) {
-				{
-					int _value = 1;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-			} else if ((blockstate.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip18 ? blockstate.getValue(_getip18) : -1) == 1) {
-				if (world.isEmptyBlock(BlockPos.containing(x, y + 1, z))) {
-					{
-						int _value = 2;
-						BlockPos _pos = BlockPos.containing(x, y, z);
-						BlockState _bs = world.getBlockState(_pos);
-						if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-							world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-					}
-					world.setBlock(BlockPos.containing(x, y + 1, z), ConcoctionModBlocks.SUNFLOWER.get().defaultBlockState(), 3);
-					{
-						int _value = 2;
-						BlockPos _pos = BlockPos.containing(x, y + 1, z);
-						BlockState _bs = world.getBlockState(_pos);
-						if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-							world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-					}
-					{
-						String _value = "upper";
-						BlockPos _pos = BlockPos.containing(x, y + 1, z);
-						BlockState _bs = world.getBlockState(_pos);
-						if (_bs.getBlock().getStateDefinition().getProperty("half") instanceof EnumProperty _enumProp && _enumProp.getValue(_value).isPresent())
-							world.setBlock(_pos, _bs.setValue(_enumProp, (Enum) _enumProp.getValue(_value).get()), 3);
-					}
-				}
-			} else if ((blockstate.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip25 ? blockstate.getValue(_getip25) : -1) == 2
-					&& (world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()) {
-				{
-					int _value = 3;
-					BlockPos _pos = BlockPos.containing(x, y + 1, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-				{
-					int _value = 3;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-			} else if ((blockstate.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip31 ? blockstate.getValue(_getip31) : -1) == 3
-					&& (world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()) {
-				{
-					int _value = 4;
-					BlockPos _pos = BlockPos.containing(x, y + 1, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-				{
-					int _value = 4;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-			} else if ((blockstate.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip37 ? blockstate.getValue(_getip37) : -1) == 4
-					&& (world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()) {
-				{
-					int _value = 5;
-					BlockPos _pos = BlockPos.containing(x, y + 1, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-				{
-					int _value = 5;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-			}
-		} else {
-			if (!world.isClientSide()) {
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockEntity _blockEntity = world.getBlockEntity(_bp);
-				BlockState _bs = world.getBlockState(_bp);
-				if (_blockEntity != null)
-					_blockEntity.getPersistentData().putDouble("attempts", (new Object() {
-						public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-							BlockEntity blockEntity = world.getBlockEntity(pos);
-							if (blockEntity != null)
-								return blockEntity.getPersistentData().getDouble(tag);
-							return -1;
-						}
-					}.getValue(world, BlockPos.containing(x, y, z), "attempts") + 1));
-				if (world instanceof Level _level)
-					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 			}
 		}
 	}
