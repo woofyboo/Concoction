255f8b35c0dc63f85b502ed86b1388b25f4797c0
woofyboo <digim7459@gmail.com>
2024-11-12 16:21:32 +0300

    bugfixes

--- src/main/java/net/mcreator/concoction/procedures/SunflowerRightClickProcedure.java
+++ src/main/java/net/mcreator/concoction/procedures/SunflowerRightClickProcedure.java
@@ -1,6 +1,6 @@
 package net.mcreator.concoction.procedures;
 
-import net.mcreator.concoction.init.ConcoctionModItems;
+import net.mcreator.concoction.init.ConcoctionModItems;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -29,68 +29,82 @@ import javax.annotation.Nullable;
 
 @EventBusSubscriber
 public class SunflowerRightClickProcedure {
-	@SubscribeEvent
-	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-		if (event.getHand() != event.getEntity().getUsedItemHand())
-			return;
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity());
-	}
+    @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (event.getHand() != event.getEntity().getUsedItemHand())
+            return;
+        execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity());
+    }
 
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-		execute(null, world, x, y, z, entity);
-	}
+    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+        execute(null, world, x, y, z, entity);
+    }
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
-		if (entity == null)
-			return;
-		if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()
-				&& ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip3 ? (world.getBlockState(BlockPos.containing(x, y, z))).getValue(_getip3) : -1) == 5
-				&& ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock().getStateDefinition().getProperty("half") instanceof EnumProperty _getep5 ? (world.getBlockState(BlockPos.containing(x, y, z))).getValue(_getep5).toString() : "")
-						.equals("upper")) {
-			{
-				int _value = 4;
-				BlockPos _pos = BlockPos.containing(x, y, z);
-				BlockState _bs = world.getBlockState(_pos);
-				if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-			}
-			if ((world.getBlockState(BlockPos.containing(x, y - 1, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()) {
-				{
-					int _value = 4;
-					BlockPos _pos = BlockPos.containing(x, y - 1, z);
-					BlockState _bs = world.getBlockState(_pos);
-					if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-						world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
-				}
-			}
-			if (!world.isClientSide()) {
-				if (world instanceof Level _level) {
-					if (!_level.isClientSide()) {
-						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.cave_vines.pick_berries")), SoundSource.PLAYERS, 1, 1);
-					} else {
-						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.cave_vines.pick_berries")), SoundSource.PLAYERS, 1, 1, false);
-					}
-				}
-			}
-			if (world instanceof ServerLevel _level) {
-				ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5), (y + 0.5), (z + 0.5), new ItemStack(ConcoctionModItems.SUNFLOWER_SEEDS.get()));
-				entityToSpawn.setPickUpDelay(10);
-				_level.addFreshEntity(entityToSpawn);
-			}
-			if (world instanceof ServerLevel _level) {
-				ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5), (y + 0.5), (z + 0.5), new ItemStack(ConcoctionModItems.SUNFLOWER_SEEDS.get()));
-				entityToSpawn.setPickUpDelay(10);
-				_level.addFreshEntity(entityToSpawn);
-			}
-			if (Math.random() < 0.5) {
-				if (world instanceof ServerLevel _level) {
-					ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5), (y + 0.5), (z + 0.5), new ItemStack(ConcoctionModItems.SUNFLOWER_SEEDS.get()));
-					entityToSpawn.setPickUpDelay(10);
-					_level.addFreshEntity(entityToSpawn);
-				}
-			}
-			if (entity instanceof LivingEntity _entity)
-				_entity.swing(InteractionHand.MAIN_HAND, true);
-		}
-	}
+    private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+        if (entity == null)
+            return;
+
+        // Проверяем, не находится ли игрок в режиме крадущегося движения
+        if (entity.isShiftKeyDown()) {
+            return; // Если игрок крадется, прекращаем выполнение
+        }
+
+        // Проверяем, что это полностью выросший подсолнух
+        if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()
+                && ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip3 ? (world.getBlockState(BlockPos.containing(x, y, z))).getValue(_getip3) : -1) == 5
+                && ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock().getStateDefinition().getProperty("half") instanceof EnumProperty _getep5 ? (world.getBlockState(BlockPos.containing(x, y, z))).getValue(_getep5).toString() : "")
+                        .equals("upper")) {
+
+            // Уменьшаем возраст подсолнуха
+            {
+                int _value = 4;
+                BlockPos _pos = BlockPos.containing(x, y, z);
+                BlockState _bs = world.getBlockState(_pos);
+                if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+                    world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+            }
+
+            // Уменьшаем возраст нижней части подсолнуха
+            if ((world.getBlockState(BlockPos.containing(x, y - 1, z))).getBlock() == ConcoctionModBlocks.SUNFLOWER.get()) {
+                {
+                    int _value = 4;
+                    BlockPos _pos = BlockPos.containing(x, y - 1, z);
+                    BlockState _bs = world.getBlockState(_pos);
+                    if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+                        world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+                }
+            }
+
+            // Воспроизводим звук
+            if (!world.isClientSide()) {
+                if (world instanceof Level _level) {
+                    if (!_level.isClientSide()) {
+                        _level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.cave_vines.pick_berries")), SoundSource.PLAYERS, 1, 1);
+                    } else {
+                        _level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.cave_vines.pick_berries")), SoundSource.PLAYERS, 1, 1, false);
+                    }
+                }
+            }
+
+            // Спавним семена подсолнуха
+            if (world instanceof ServerLevel _level) {
+                ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5), (y + 0.5), (z + 0.5), new ItemStack(ConcoctionModItems.SUNFLOWER_SEEDS.get()));
+                entityToSpawn.setPickUpDelay(10);
+                _level.addFreshEntity(entityToSpawn);
+            }
+
+            // Дополнительный шанс на выпадение семян
+            if (Math.random() < 0.5) {
+                if (world instanceof ServerLevel _level) {
+                    ItemEntity entityToSpawn = new ItemEntity(_level, (x + 0.5), (y + 0.5), (z + 0.5), new ItemStack(ConcoctionModItems.SUNFLOWER_SEEDS.get()));
+                    entityToSpawn.setPickUpDelay(10);
+                    _level.addFreshEntity(entityToSpawn);
+                }
+            }
+
+            // Анимация махания рукой
+            if (entity instanceof LivingEntity _entity)
+                _entity.swing(InteractionHand.MAIN_HAND, true);
+        }
+    }
 }
