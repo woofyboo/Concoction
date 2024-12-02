4ea95d9b06a2ea183d97c7d0ed40b11de883e084
woofyboo <digim7459@gmail.com>
2024-12-02 22:43:48 +0300

    wild_onion_model_fix

--- src/main/java/net/mcreator/concoction/block/WildOnionBlock.java
+++ src/main/java/net/mcreator/concoction/block/WildOnionBlock.java
@@ -1,6 +1,10 @@
 
 package net.mcreator.concoction.block;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,14 +14,24 @@ import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.concoction.init.ConcoctionModItems;
+
 public class WildOnionBlock extends FlowerBlock {
 	public WildOnionBlock() {
 		super(MobEffects.MOVEMENT_SPEED, 0, BlockBehaviour.Properties.of().mapColor(MapColor.NONE).sound(SoundType.GRASS).instabreak().noCollission().offsetType(BlockBehaviour.OffsetType.NONE).pushReaction(PushReaction.DESTROY));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		Vec3 offset = state.getOffset(world, pos);
+		return box(2, 0, 2, 14, 13, 14).move(offset.x, offset.y, offset.z);
 	}
 
 	@Override
@@ -28,6 +42,11 @@ public class WildOnionBlock extends FlowerBlock {
 	@Override
 	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 60;
+	}
+
+	@Override
+	public ItemStack getCloneItemStack(BlockState state, HitResult target, LevelReader world, BlockPos pos, Player player) {
+		return new ItemStack(ConcoctionModItems.ONION.get());
 	}
 
 	private boolean canPlantTypeSurvive(BlockState state, LevelReader world, BlockPos pos) {
