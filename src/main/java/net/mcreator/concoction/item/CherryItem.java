6b9ac7c47e2f5183204b13a9d0594afcdadfcc8f
Sebsish <se6sish@gmail.com>
2024-12-03 00:51:14 +0300

    add suplings spawn on despawn pinecon and cherry

--- src/main/java/net/mcreator/concoction/item/CherryItem.java
+++ src/main/java/net/mcreator/concoction/item/CherryItem.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.concoction.item;
 
+import net.mcreator.concoction.init.ConcoctionModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
@@ -9,9 +12,25 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.item.ItemExpireEvent;
 
+@EventBusSubscriber
 public class CherryItem extends Item {
 	public CherryItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationModifier(0.3f).build()));
+	}
+	@SubscribeEvent
+	public static void itemEntityDespawn(ItemExpireEvent event) {
+		if (!event.getEntity().level().isClientSide() &&
+				event.getEntity().getItem().getItem() == ConcoctionModItems.CHERRY.get()) {
+			if (event.getEntity().level().getBlockState(event.getEntity().blockPosition()).getBlock() == Blocks.AIR &&
+					event.getEntity().level().getBlockState(event.getEntity().blockPosition().below()).is(BlockTags.create(ResourceLocation.parse("minecraft:dirt")))) {
+				event.getEntity().level().setBlock(event.getEntity().blockPosition(), Blocks.CHERRY_SAPLING.defaultBlockState(), 3);
+			}
+//			if (itemSt.getCount() < 4) {}
+		}
 	}
 }
--- src/main/java/net/mcreator/concoction/item/PineconeItem.java
+++ src/main/java/net/mcreator/concoction/item/PineconeItem.java
@@ -1,11 +1,34 @@
 
 package net.mcreator.concoction.item;
 
+import net.mcreator.concoction.init.ConcoctionModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.item.ItemExpireEvent;
 
+@EventBusSubscriber
 public class PineconeItem extends Item {
 	public PineconeItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@SubscribeEvent
+	public static void itemEntityDespawn(ItemExpireEvent event) {
+		if (!event.getEntity().level().isClientSide() &&
+				event.getEntity().getItem().getItem() == ConcoctionModItems.PINECONE.get()) {
+			if (event.getEntity().level().getBlockState(event.getEntity().blockPosition()).getBlock() == Blocks.AIR &&
+					event.getEntity().level().getBlockState(event.getEntity().blockPosition().below()).is(BlockTags.create(ResourceLocation.parse("minecraft:dirt")))) {
+				event.getEntity().level().setBlock(event.getEntity().blockPosition(), Blocks.SPRUCE_SAPLING.defaultBlockState(), 3);
+			}
+//			if (itemSt.getCount() < 4) {}
+		}
 	}
 }
