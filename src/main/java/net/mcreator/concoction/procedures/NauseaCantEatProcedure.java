185ccb9435ce66c22091d62677c8b3556095d2ba
woofyboo <digim7459@gmail.com>
2024-12-10 09:19:09 +0300

    nausea_and_pepper_textures

---
+++ src/main/java/net/mcreator/concoction/procedures/NauseaCantEatProcedure.java
@@ -0,0 +1,45 @@
+package net.mcreator.concoction.procedures;
+
+import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.ICancellableEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
+public class NauseaCantEatProcedure {
+	@SubscribeEvent
+	public static void onUseItemStart(LivingEntityUseItemEvent.Start event) {
+		if (event.getEntity() != null) {
+			execute(event, event.getEntity(), event.getItem());
+		}
+	}
+
+	public static void execute(Entity entity, ItemStack itemstack) {
+		execute(null, entity, itemstack);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(MobEffects.CONFUSION)) {
+			if (itemstack.is(ItemTags.create(ResourceLocation.parse("c:foods")))) {
+				if (entity instanceof Player _player)
+					_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
+				if (event instanceof ICancellableEvent _cancellable) {
+					_cancellable.setCanceled(true);
+				}
+			}
+		}
+	}
+}
