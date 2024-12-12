7dfeda2af1cee9c9ac72a87478bcda58d7b69618
Sebsish <se6sish@gmail.com>
2024-12-13 00:30:22 +0300

    Update SpicyOnEffectActiveTickProcedure.java

--- src/main/java/net/mcreator/concoction/procedures/SpicyOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/concoction/procedures/SpicyOnEffectActiveTickProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.concoction.procedures;
 
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -14,9 +19,18 @@ public class SpicyOnEffectActiveTickProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(ConcoctionModMobEffects.SPICY) ? _livEnt.getEffect(ConcoctionModMobEffects.SPICY).getDuration() : 0) % 60 == 0) {
-			entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("concoction:spicy_damage")))),
-					(float) (((entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(ConcoctionModMobEffects.SPICY) ? _livEnt.getEffect(ConcoctionModMobEffects.SPICY).getAmplifier() : 0) + 1) * 1));
+		if (entity instanceof LivingEntity livEnt) {
+			if ((livEnt.hasEffect(ConcoctionModMobEffects.SPICY) ? livEnt.getEffect(ConcoctionModMobEffects.SPICY).getDuration() : 0) % 60 == 0) {
+				livEnt.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("concoction:spicy_damage")))),
+						(float) (((livEnt.hasEffect(ConcoctionModMobEffects.SPICY) ? livEnt.getEffect(ConcoctionModMobEffects.SPICY).getAmplifier() : 0) + 1) * 1));
+				if (world instanceof Level pLevel && !pLevel.isClientSide()) {
+					livEnt.getActiveEffects().stream().map(effect -> new Pair<>(effect.getEffect(), effect.getEffect().value().getCategory())).
+							filter(pair -> pair.getSecond().equals(MobEffectCategory.HARMFUL)).forEach(pair -> {
+								livEnt.removeEffect(pair.getFirst());
+					});
+				}
+			}
 		}
 	}
 }
+
