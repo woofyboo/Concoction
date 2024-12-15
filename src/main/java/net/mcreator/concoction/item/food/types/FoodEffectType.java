fc81dee35733395148a2d7ae6bd5967637bc18d6
Sebsish <se6sish@gmail.com>
2024-12-15 16:46:25 +0300

    add glow and instability food effects

--- src/main/java/net/mcreator/concoction/item/food/types/FoodEffectType.java
+++ src/main/java/net/mcreator/concoction/item/food/types/FoodEffectType.java
@@ -4,11 +4,14 @@ import net.mcreator.concoction.init.ConcoctionModMobEffects;
 import net.mcreator.concoction.init.ConcoctionModPotions;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 
 public enum FoodEffectType implements StringRepresentable {
     SWEET("sweet"),
     SPICY("spicy"),
-    MINTY("minty");
+    MINTY("minty"),
+    GLOW("glow"),
+    INSTABILITY("instability");
 
     private final String name;
     private FoodEffectType(String name) {
@@ -25,6 +28,8 @@ public enum FoodEffectType implements StringRepresentable {
             case "sweet" -> SWEET;
             case "spicy" -> SPICY;
             case "minty" -> MINTY;
+            case "glow" -> GLOW;
+            case "instability" -> INSTABILITY;
             default -> throw new IllegalArgumentException("Invalid name: " + name);
         };
     }
@@ -34,6 +39,8 @@ public enum FoodEffectType implements StringRepresentable {
             case SWEET -> new MobEffectInstance(ConcoctionModMobEffects.SWEETNESS, duration*20, level-1);
             case SPICY -> new MobEffectInstance(ConcoctionModMobEffects.SPICY, duration*20, level-1);
             case MINTY -> new MobEffectInstance(ConcoctionModMobEffects.MINTY_BREATH, duration*20, level-1);
+            case GLOW ->  new MobEffectInstance(MobEffects.GLOWING, duration*20, level-1);
+            case INSTABILITY -> new MobEffectInstance(ConcoctionModMobEffects.INSTABILITY, duration*20, level-1);
         };
     }
 }
