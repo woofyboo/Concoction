26154e9b1d37695259d7e6731269e42b2e42b2fd
woofyboo <digim7459@gmail.com>
2025-01-06 18:20:44 +0300

    rotatable_corn_block

--- src/main/java/net/mcreator/concoction/block/CornBlockBlock.java
+++ src/main/java/net/mcreator/concoction/block/CornBlockBlock.java
@@ -4,14 +4,12 @@ package net.mcreator.concoction.block;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -20,13 +18,13 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class CornBlockBlock extends Block {
-	public static final DirectionProperty FACING = DirectionalBlock.FACING;
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
 	public static final EnumProperty<TypeProperty> TYPE = EnumProperty.create("type", TypeProperty.class);
 	public static final IntegerProperty CONFIGURATION = IntegerProperty.create("configuration", 0, 44);
 
 	public CornBlockBlock() {
-		super(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).sound(SoundType.WART_BLOCK).strength(0.8f, 0.8f));
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(TYPE, TypeProperty.DEFAULT).setValue(CONFIGURATION, 0));
+		super(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).sound(SoundType.CHERRY_WOOD).strength(0.8f, 0.8f));
+		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y).setValue(TYPE, TypeProperty.DEFAULT).setValue(CONFIGURATION, 0));
 	}
 
 	@Override
@@ -37,20 +35,24 @@ public class CornBlockBlock extends Block {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
-		builder.add(FACING, TYPE, CONFIGURATION);
+		builder.add(AXIS, TYPE, CONFIGURATION);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return super.getStateForPlacement(context).setValue(FACING, context.getClickedFace()).setValue(TYPE, TypeProperty.DEFAULT).setValue(CONFIGURATION, 0);
+		return super.getStateForPlacement(context).setValue(AXIS, context.getClickedFace().getAxis()).setValue(TYPE, TypeProperty.DEFAULT).setValue(CONFIGURATION, 0);
 	}
 
+	@Override
 	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
-	}
-
-	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+		if (rot == Rotation.CLOCKWISE_90 || rot == Rotation.COUNTERCLOCKWISE_90) {
+			if (state.getValue(AXIS) == Direction.Axis.X) {
+				return state.setValue(AXIS, Direction.Axis.Z);
+			} else if (state.getValue(AXIS) == Direction.Axis.Z) {
+				return state.setValue(AXIS, Direction.Axis.X);
+			}
+		}
+		return state;
 	}
 
 	@Override
