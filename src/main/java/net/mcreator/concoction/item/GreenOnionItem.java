391de73734a45768c2072e2d7c454d8fb7690789
woofyboo <digim7459@gmail.com>
2025-02-17 10:27:28 +0300

    advancements_revamp01

--- src/main/java/net/mcreator/concoction/item/GreenOnionItem.java
+++ src/main/java/net/mcreator/concoction/item/GreenOnionItem.java
@@ -7,6 +7,6 @@ import net.minecraft.world.food.FoodProperties;
 
 public class GreenOnionItem extends Item {
 	public GreenOnionItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(1).saturationModifier(0.3f).alwaysEdible().build()));
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(1).saturationModifier(0.3f).build()));
 	}
 }
