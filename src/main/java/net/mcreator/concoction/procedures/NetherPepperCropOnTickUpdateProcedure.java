2e1e94a6c40e72d02cbc69cb9bdc43a42dfe6b3d
woofyboo <digim7459@gmail.com>
2025-03-09 22:46:04 +0300

    reapper_final_version

--- src/main/java/net/mcreator/concoction/procedures/NetherPepperCropOnTickUpdateProcedure.java
+++ src/main/java/net/mcreator/concoction/procedures/NetherPepperCropOnTickUpdateProcedure.java
@@ -12,13 +12,19 @@ import net.minecraft.core.BlockPos;
 
 public class NetherPepperCropOnTickUpdateProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.addParticle(ParticleTypes.SOUL, (x + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)), (y + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)), (z + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)),
-				(Mth.nextDouble(RandomSource.create(), -0.13, 0.13)), (Mth.nextDouble(RandomSource.create(), 0.13, 0.2)), (Mth.nextDouble(RandomSource.create(), -0.13, 0.13)));
-		if (world instanceof Level _level) {
-			if (!_level.isClientSide()) {
-				_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("particle.soul_escape")), SoundSource.BLOCKS, 1, (float) Math.random());
-			} else {
-				_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("particle.soul_escape")), SoundSource.BLOCKS, 1, (float) Math.random(), false);
+		if (17950 <= world.dayTime() && 18050 >= world.dayTime()) {
+			if (Math.random() < 0.5) {
+				world.addParticle(ParticleTypes.SOUL, (x + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)), (y + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)), (z + Mth.nextDouble(RandomSource.create(), 0.2, 0.8)),
+						(Mth.nextDouble(RandomSource.create(), -0.13, 0.13)), (Mth.nextDouble(RandomSource.create(), 0.13, 0.2)), (Mth.nextDouble(RandomSource.create(), -0.13, 0.13)));
+			}
+			if (Math.random() < 0.1) {
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("particle.soul_escape")), SoundSource.BLOCKS, 1, (float) Math.random());
+					} else {
+						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("particle.soul_escape")), SoundSource.BLOCKS, 1, (float) Math.random(), false);
+					}
+				}
 			}
 		}
 	}
