bbf5371d9663aaf227d2fa4e219e18764eb75d9e
woofyboo <digim7459@gmail.com>
2025-03-21 02:50:25 +0300

    bugfix_v3

--- src/main/java/net/mcreator/concoction/procedures/FieryTouchActiveTickProcedure.java
+++ src/main/java/net/mcreator/concoction/procedures/FieryTouchActiveTickProcedure.java
@@ -4,13 +4,9 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
-import net.minecraft.advancements.AdvancementProgress;
-import net.minecraft.advancements.AdvancementHolder;
 
 import net.mcreator.concoction.init.ConcoctionModMobEffects;
 
@@ -52,16 +48,6 @@ public class FieryTouchActiveTickProcedure {
 				_entity.removeEffect(ConcoctionModMobEffects.FROST_TOUCH);
 			if (entity instanceof LivingEntity _entity)
 				_entity.removeEffect(ConcoctionModMobEffects.FIERY_TOUCH);
-			if (entity instanceof ServerPlayer _player) {
-				AdvancementHolder _adv = _player.server.getAdvancements().get(ResourceLocation.parse("concoction:screenshot"));
-				if (_adv != null) {
-					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-					if (!_ap.isDone()) {
-						for (String criteria : _ap.getRemainingCriteria())
-							_player.getAdvancements().award(_adv, criteria);
-					}
-				}
-			}
 		}
 	}
 }
