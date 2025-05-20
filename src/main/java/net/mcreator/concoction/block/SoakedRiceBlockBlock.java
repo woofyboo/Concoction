644b43f7c3a6a1a03a89064eb82e5c6dd599bec0
woofyboo <digim7459@gmail.com>
2025-05-20 23:37:36 +0300

    Falling Soaked Rice V1

--- src/main/java/net/mcreator/concoction/block/SoakedRiceBlockBlock.java
+++ src/main/java/net/mcreator/concoction/block/SoakedRiceBlockBlock.java
@@ -1,25 +1,33 @@
-
 package net.mcreator.concoction.block;
 
-import net.mcreator.concoction.init.ConcoctionModBlocks;
-import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.WetSpongeBlock;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.particles.ParticleTypes;
+import net.mcreator.concoction.init.ConcoctionModBlocks;
 
-public class SoakedRiceBlockBlock extends Block {
+
+import com.mojang.serialization.MapCodec;
+
+public class SoakedRiceBlockBlock extends FallingBlock {
+	public static final MapCodec<SoakedRiceBlockBlock> CODEC = simpleCodec(properties -> new SoakedRiceBlockBlock());
+
+	@Override
+	public MapCodec<SoakedRiceBlockBlock> codec() {
+		return CODEC;
+	}
+
 	public SoakedRiceBlockBlock() {
 		super(BlockBehaviour.Properties.of().mapColor(MapColor.SNOW).sound(SoundType.MUD).strength(0.5f));
 	}
@@ -30,51 +38,44 @@ public class SoakedRiceBlockBlock extends Block {
 	}
 
 	@Override
-	protected void onPlace(BlockState p_58229_, Level p_58230_, BlockPos p_58231_, BlockState p_58232_, boolean p_58233_) {
-		if (p_58230_.dimensionType().ultraWarm()) {
-			p_58230_.setBlock(p_58231_, ConcoctionModBlocks.RICE_BLOCK.get().defaultBlockState(), 3);
-			p_58230_.levelEvent(2009, p_58231_, 0);
-			p_58230_.playSound((Player)null, p_58231_, SoundEvents.WET_SPONGE_DRIES, SoundSource.BLOCKS, 1.0F, (1.0F + p_58230_.getRandom().nextFloat() * 0.2F) * 0.7F);
-		}
-
+public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
+	if (level.dimensionType().ultraWarm()) {
+		level.setBlock(pos, ConcoctionModBlocks.RICE_BLOCK.get().defaultBlockState(), 3);
+		level.levelEvent(2009, pos, 0);
+		level.playSound((Player) null, pos, SoundEvents.WET_SPONGE_DRIES, SoundSource.BLOCKS, 1.0F,
+				(1.0F + level.getRandom().nextFloat() * 0.2F) * 0.7F);
+	} else {
+		level.scheduleTick(pos, this, this.getDelayAfterPlace());
 	}
+}
+
 
 	@Override
-	public void animateTick(BlockState p_222682_, Level p_222683_, BlockPos p_222684_, RandomSource p_222685_) {
-		Direction direction = Direction.getRandom(p_222685_);
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+		Direction direction = Direction.getRandom(random);
 		if (direction != Direction.UP) {
-			BlockPos blockpos = p_222684_.relative(direction);
-			BlockState blockstate = p_222683_.getBlockState(blockpos);
-			if (!p_222682_.canOcclude() || !blockstate.isFaceSturdy(p_222683_, blockpos, direction.getOpposite())) {
-				double d0 = (double)p_222684_.getX();
-				double d1 = (double)p_222684_.getY();
-				double d2 = (double)p_222684_.getZ();
+			BlockPos neighborPos = pos.relative(direction);
+			BlockState neighborState = level.getBlockState(neighborPos);
+			if (!state.canOcclude() || !neighborState.isFaceSturdy(level, neighborPos, direction.getOpposite())) {
+				double x = pos.getX();
+				double y = pos.getY();
+				double z = pos.getZ();
 				if (direction == Direction.DOWN) {
-					d1 -= 0.05;
-					d0 += p_222685_.nextDouble();
-					d2 += p_222685_.nextDouble();
+					y -= 0.05;
+					x += random.nextDouble();
+					z += random.nextDouble();
 				} else {
-					d1 += p_222685_.nextDouble() * 0.8;
+					y += random.nextDouble() * 0.8;
 					if (direction.getAxis() == Direction.Axis.X) {
-						d2 += p_222685_.nextDouble();
-						if (direction == Direction.EAST) {
-							++d0;
-						} else {
-							d0 += 0.05;
-						}
+						z += random.nextDouble();
+						x += direction == Direction.EAST ? 1.0 : 0.05;
 					} else {
-						d0 += p_222685_.nextDouble();
-						if (direction == Direction.SOUTH) {
-							++d2;
-						} else {
-							d2 += 0.05;
-						}
+						x += random.nextDouble();
+						z += direction == Direction.SOUTH ? 1.0 : 0.05;
 					}
 				}
-
-				p_222683_.addParticle(ParticleTypes.DRIPPING_WATER, d0, d1, d2, (double)0.0F, (double)0.0F, (double)0.0F);
+				level.addParticle(ParticleTypes.DRIPPING_WATER, x, y, z, 0.0F, 0.0F, 0.0F);
 			}
 		}
-
 	}
 }
