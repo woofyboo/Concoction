58af4d59a5dea932901df9db632b59e1c97fce95
woofyboo <digim7459@gmail.com>
2025-11-02 15:36:01 +0300

    Nisholda

---
+++ src/main/java/net/mcreator/concoction/item/NisholdaItem.java
@@ -0,0 +1,40 @@
+
+package net.mcreator.concoction.item;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+
+import net.mcreator.concoction.item.food.types.FoodEffectComponent;
+import net.mcreator.concoction.item.food.types.FoodEffectType;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import static net.mcreator.concoction.init.ConcoctionModDataComponents.FOOD_EFFECT;
+import static net.mcreator.concoction.init.ConcoctionModDataComponents.*;
+
+public class NisholdaItem extends Item {
+	public NisholdaItem() {
+		super(new Item.Properties().stacksTo(16).component(FOOD_EFFECT.value(), new FoodEffectComponent(FoodEffectType.CREAMY, 1, 90, true)).component(FOOD_EFFECT_2.value(), new FoodEffectComponent(FoodEffectType.SWEET, 1, 30, true))
+		.rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(6).saturationModifier(0.8f).build()));
+	}
+
+    @Override
+    public @NotNull ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+        ItemStack retval = new ItemStack(Items.BOWL);
+        super.finishUsingItem(itemstack, world, entity);
+        if (itemstack.isEmpty()) {
+            return retval;
+        } else {
+            if (entity instanceof Player player && !player.getAbilities().instabuild) {
+                if (!player.getInventory().add(retval))
+                    player.drop(retval, false);
+            }
+            return itemstack;
+        }
+    }
+}
