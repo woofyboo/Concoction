1a1bcec3e90ee84631836eab8b1db9902793e0fc
woofyboo <digim7459@gmail.com>
2025-11-07 20:15:16 +0300

    Descriptions and Tastes Fix

--- src/main/java/net/mcreator/concoction/item/PumpkinPieSliceItem.java
+++ src/main/java/net/mcreator/concoction/item/PumpkinPieSliceItem.java
@@ -1,15 +1,20 @@
 
 package net.mcreator.concoction.item;
 
+import net.mcreator.concoction.item.food.types.FoodEffectComponent;
+import net.mcreator.concoction.item.food.types.FoodEffectType;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.LivingEntity;
 
-public class PumpkinPieSliceItem extends Item {
+import static net.mcreator.concoction.init.ConcoctionModDataComponents.*;
+
+public class PumpkinPieSliceItem extends TastefulItem {
 	public PumpkinPieSliceItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationModifier(0.6f).build()));
+		super(new Item.Properties().stacksTo(64).
+                component(FOOD_EFFECT.value(), new FoodEffectComponent(FoodEffectType.SWEET, 2, 6, true)).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationModifier(0.6f).build()));
 	}
 
 	@Override
